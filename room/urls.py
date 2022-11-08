c96466d336004ed7d31bb4fb0f1df4c82300282d
Geovanny Reyes <geovanny.reyes@correounivalle.edu.co>
2022-11-08 09:35:35 -0500

    View layout & routes

--- room/urls.py
+++ room/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('explore/', views.explore, name='explore'),
+    path('login/', views.login, name='login'),
+    path('room/', views.room, name='room'),
+    path('user/', views.user, name='user'),
 ]
--- room/views.py
+++ room/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'room/home.html')
+
+def explore(request):
+    return render(request, 'room/explore.html')
+
+def login(request):
+    return render(request, 'room/login.html')
+
+def room(request):
+    return render(request, 'room/room.html')
+
+def user(request):
+    return render(request, 'room/user.html')
